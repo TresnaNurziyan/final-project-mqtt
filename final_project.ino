80f014ff9cd81a67753b021ce01d920c7da7cdb4
Tresna Nurziyan <tresna.nurziyan@gmail.com>
2021-12-26 15:11:39 +0700

    adding mqtt protocol

--- final_project.ino
+++ final_project.ino
@@ -1,25 +1,139 @@
-const int led = 14;
-const int sensor = 12;
-const int delayTime = 50;
-int firstTime = 0;
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "GRYFFINDOR";
+const char* password = "Al0homor4";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+#define ledPin 4
+#define triggerPin  15
+#define echoPin     13
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // ... and resubscribe
+      client.subscribe("inSensorBanjir");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
-  pinMode(led, OUTPUT);
-  pinMode(sensor, INPUT);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  pinMode(ledPin, OUTPUT);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int data = digitalRead(sensor);
-    if(data == HIGH)
-    {
-        digitalWrite(led, HIGH);
-        firstTime = millis();
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  long duration, jarak;
+  
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(triggerPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  jarak = (duration/2) / 29.1;
+  
+  Serial.println("jarak :");
+  Serial.print(jarak);
+  Serial.println(" cm");
+  delay(1000);
+  
+  if (jarak < 10) 
+  { 
+    Serial.println("Motion detected");
+    digitalWrite(ledPin, HIGH);
+
+    unsigned long now = millis();
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+      ++value;
+      snprintf (msg, MSG_BUFFER_SIZE, "Jarak air pada batas ambang bendungan: #%ld cm", jarak);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish("outSensorBanjir", msg);
     }
-    if(millis()- firstTime > delayTime)
-    {
-        digitalWrite(led, LOW);
-    }
+  } 
+  else {
+    digitalWrite(ledPin, LOW);
+  }
 }
